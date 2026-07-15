a1090ce55991c8d7bca38fde89c2fd72ee3f827a
ndjassapfranck7-gif <ndjassapfranck7@gmail.com>
2026-07-15 14:54:43 +0100

    19-commit

--- Formulaire/JS.js
+++ Formulaire/JS.js
@@ -1,15 +1,15 @@
 const formulaire = document.getElementById("nform");
 const email = document.getElementById("email");
 const message = document.getElementById("message");
-const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+const emailRegex = /^[^\s@]+@gmail\..+$/;
 
 formulaire.addEventListener("submit", function (e) {
     if (!emailRegex.test(email.value.trim())) {
         e.preventDefault();
-        message.textContent = "veuillez entre un email valide";
+        message.textContent = "Veuillez entrer un email valide";
         message.style.color = "red";
     } else {
-        message.textContent = "Email valide";
+        message.textContent = "Email Gmail valide";
         message.style.color = "green";
     }
 });
